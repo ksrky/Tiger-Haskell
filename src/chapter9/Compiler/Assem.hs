e61d29a281da6dc07e720d2964e8e8c600ce0a3a
ksrk02 <xgrk9475@gmail.com>
2022-05-03 16:47:03 +0000

    chapter: complete format in Assem

--- src/chapter9/Compiler/Assem.hs
+++ src/chapter9/Compiler/Assem.hs
@@ -4,22 +4,26 @@ import qualified Common.Symbol as Symbol
 import qualified Common.Temp as Temp
 
 data Instr
-        = OPER {assem :: String, dst :: [Temp.Temp], src :: [Temp.Temp], jump :: Maybe Temp.Label}
+        = OPER {assem :: String, dst :: [Temp.Temp], src :: [Temp.Temp], jump :: Maybe [Temp.Label]}
         | LABEL {assem :: String, lab :: Temp.Label}
         | MOVE {assem :: String, dst' :: Temp.Temp, src' :: Temp.Temp}
 
 format :: (Temp.Temp -> String) -> Instr -> String
-format saytemp (OPER assem dst src Nothing) = error "not implemented"
-format saytemp (OPER assem dst src (Just j)) = error "not implemented"
-format saytemp (LABEL assem _) = error "not implemented"
-format saytemp (MOVE assem dst src) = error "not implemented"
+format saytemp = format'
     where
+        format' :: Instr -> String
+        format' (OPER assem dst src Nothing) = speak assem dst src []
+        format' (OPER assem dst src (Just j)) = speak assem dst src j
+        format' (LABEL assem _) = assem
+        format' (MOVE assem dst src) = speak assem [dst] [src] []
+        speak :: String -> [Temp.Temp] -> [Temp.Temp] -> [Temp.Label] -> String
         speak assem dst src jump = f assem
             where
                 saylab = Symbol.name
-                f ('`' : 's' : i : rest) = saytemp src !! read [i] : f rest
-                f ('`' : 'd' : i : rest) = saytemp dst !! read [i] : f rest
-                f ('`' : 'j' : i : rest) = saylab jump !! read [i] : f rest
+                f :: String -> String
+                f ('`' : 's' : i : rest) = saytemp (src !! read [i]) ++ f rest
+                f ('`' : 'd' : i : rest) = saytemp (dst !! read [i]) ++ f rest
+                f ('`' : 'j' : i : rest) = saylab (jump !! read [i]) ++ f rest
                 f ('`' : '`' : rest) = '`' : f rest
                 f ('`' : _ : rest) = error "impossible bad assem format"
                 f (c : rest) = c : f rest
